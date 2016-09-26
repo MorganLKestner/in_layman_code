49d269ae6473b12ad8818a8257adf7ae127836ab
Morgan Kestner <morganleekestner@gmail.com>
2016-09-26 19:58:47 -0400

    omniauth heroku debug route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
 
 	root to: "articles#index"
 	resources :articles
-	
-	get "/auth/:provider/callback" => "sessions#create"
+
+	get "https://github.com/login/oauth/authorize" => "sessions#create"
+	post "https://github.com/login/oauth/access_token"
+
+	# get "/auth/:provider/callback" => "sessions#create"
 	get "/signout" => "sessions#destroy", :as => :signout
 end
