d4a232ccc9817cf659db08fef5a089493247132f
Morgan Kestner <morganleekestner@gmail.com>
2016-09-26 16:14:27 -0400

    added language select to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   include SearchCop
 
   search_scope :search do
-    attributes :title, :content
+    attributes :title, :content, :rcode
   end
 
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926162733) do
+ActiveRecord::Schema.define(version: 20160926193754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160926162733) do
     t.datetime "updated_at", null: false
     t.string   "rcode"
     t.string   "content"
+    t.string   "language"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
