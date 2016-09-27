a4d5bbc19e31ed86b72fd9a1bbf0ac2fbd414f37
Morgan Kestner <morganleekestner@gmail.com>
2016-09-26 21:17:08 -0400

    fixing routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
 	root to: "articles#index"
 	resources :articles
 
-	get "https://github.com/login/oauth/authorize" => "sessions#create"
-	post "https://github.com/login/oauth/access_token"
-
 	 get "/auth/:provider/callback" => "sessions#create"
 	get "/signout" => "sessions#destroy", :as => :signout
 end
